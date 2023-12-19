8fe6b83202d70d0eae71bd4fb6b67de6a1c9bc92
Akmuhammet Ashyralyyev <akmuhammet99@gmail.com>
2023-12-19 21:13:25 +0300

    main query func implemented

--- query.py
+++ query.py
@@ -8,6 +8,7 @@ from sklearn.metrics.pairwise import cosine_similarity
 from rank_bm25 import BM25Okapi
 from flask import Flask, request, jsonify
 import os
+import traceback
 
 
 # Create Flask application
@@ -19,20 +20,20 @@ logging.getLogger().setLevel(logging.INFO)
 
 # Get movie path
 CWD = os.getcwd()
-movies_path = os.path.join(CWD, "dataset", "IMDb movies.csv")
+movies_path = os.path.join(CWD, "dataset", "IMDb movies preprocessed.csv")
 
 # Get movies dataset
-movies = pd.read_csv(os.path.join(CWD, "dataset", "IMDb movies.csv"))
+movies = pd.read_csv(os.path.join(CWD, "dataset", "IMDb movies preprocessed.csv"))
 # Remove nan
 movies = movies.where(pd.notnull(movies), "")
 
 # Initialize TfidfVectorizer
 tfidfvectorizer = TfidfVectorizer(analyzer='word',stop_words= 'english')
-tfidf_wm = tfidfvectorizer.fit_transform(movies["description"])
+tfidf_wm = tfidfvectorizer.fit_transform(movies["detailed_description"])
 
 # Initialize BM25Okapi
-tokenized_docs = [document.split(" ") for document in movies["description"]]
-bm25_wm = BM25Okapi(tokenized_docs)
+tokenized_docs = [document.split(" ") for document in movies["detailed_description"]]
+bm25_wm = BM25Okapi(movies["detailed_description"])
 
 
 
@@ -42,22 +43,33 @@ bm25_wm = BM25Okapi(tokenized_docs)
 #-------------------------------------------------------------------------------------------
 #-------------------------------------------------------------------------------------------
 def query(sentence):
-    # Scores will be calculated with cosine similarity function
+    try:
 
-    # Get scores from TF-IDF
-    query_scores_tfidf = tfidfvectorizer.transform([sentence])
-    query_scores_tfidf = cosine_similarity(query_scores_tfidf, tfidf_wm).flatten()
+        # Scores will be calculated with cosine similarity function
 
-    # Get scores from BM25
-    query_scores_bm25 = bm25_wm.get_scores(sentence)
-    # Normalize scores
-    query_scores_bm25 = query_scores_bm25 /  np.linalg.norm(query_scores_bm25)
+        # Get scores from TF-IDF
+        query_scores_tfidf = tfidfvectorizer.transform([sentence])
+        query_scores_tfidf = cosine_similarity(query_scores_tfidf, tfidf_wm).flatten()
+        query_scores_tfidf_indeces = np.argpartition(query_scores_tfidf, -10)[-10:]
+        query_scores_tfidf_10_values = movies.iloc[query_scores_tfidf_indeces]
 
-    top10_tfidf = heapq.nlargest(10, np.ndenumerate(query_scores_tfidf), key=lambda x: x[1])
-    top10_bm25 = heapq.nlargest(10, np.ndenumerate(query_scores_bm25), key=lambda x: x[1])
+        # Get scores from BM25
+        query_scores_bm25 = bm25_wm.get_scores(sentence)
+        query_scores_bm25 = (query_scores_bm25 - np.min(query_scores_bm25)) / (np.max(query_scores_bm25) - np.min(query_scores_bm25) )
+        query_scores_bm25_indeces = np.argpartition(query_scores_bm25, -10)[-20:]
+        query_scores_bm25_10_movies = movies.iloc[query_scores_bm25_indeces]
+
+        logging.warn(query_scores_bm25_indeces)
+        logging.warn(query_scores_bm25_10_movies["imdb_title_id"])
+        logging.warn(query_scores_bm25)
+        logging.warn(query_scores_bm25_10_movies)
+
+        return {"movies": query_scores_tfidf_10_values.to_dict(orient="records")}
     
-    return({"tfidf": top10_tfidf, "bm25": top10_bm25})
-
+    except Exception as e:
+        traceback_str = traceback.format_exc()
+        logging.error(f"An error occurred: {e}\n{traceback_str}")
+        return jsonify({"error": str(e), "traceback": traceback_str}), 500
 
 #-------------------------------------------------------------------------------------------
 #-------------------------------------------------------------------------------------------
