c6184fb63f48a42fe3b662395d53d5eddbcff135
Akmuhammet Ashyralyyev <akmuhammet99@gmail.com>
2023-12-19 21:13:05 +0300

    preprocess new column added for detailed

--- preprocess.py
+++ preprocess.py
@@ -14,16 +14,12 @@ movies = movies.where(pd.notnull(movies), "")
 
 #imdb_title_id,title,original_title,year,date_published,genre,duration,country,language,director,writer,production_company,actors,description,avg_vote,votes,budget,usa_gross_income,worlwide_gross_income,metascore,reviews_from_users,reviews_from_critics
 
-movies["description"] = "the title is " + movies["title"] + \
+movies["detailed_description"] = "the title is " + movies["title"] + \
                             ", original title is " + movies["original_title"] + \
                                     ", genre is " + movies["genre"] + \
                                         ", director is " + movies["director"] + \
                                             ", actors are " + movies["actors"] + \
                                                 ", movie plot is " + movies["description"]
-
-movies = movies[["imdb_title_id", "description"]]
-
-print(movies)
 
 movies.to_csv(output_path, index=False)
                             
